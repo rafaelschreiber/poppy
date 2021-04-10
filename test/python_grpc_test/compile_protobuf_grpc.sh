60f6e047cddfdbc4c813215e2581cefe9218ab8b
Rafael Schreiber <rafael@backend.works>
2021-04-10 03:49:31 +0200

    remove test

--- test/python_grpc_test/compile_protobuf_grpc.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-
-cd $SCRIPTPATH
-
-python -m grpc_tools.protoc -I../../protos --python_out=. --grpc_python_out=. ../../protos/pop3client.proto
